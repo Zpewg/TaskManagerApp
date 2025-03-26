65bf533347424a20ce5d4277b20cf20c85135d05
Zpewg <mstefanandrei129@gmail.com>
2025-03-26 14:22:22 +0200

    Password collumn update

---
+++ Task Manager/Migrations/20250326103433_PasswordCollumnUpdate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task_Manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordCollumnUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "user",
+                type: "varchar(90)",
+                maxLength: 90,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(16)",
+                oldMaxLength: 16)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "user",
+                type: "varchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(90)",
+                oldMaxLength: 90)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
